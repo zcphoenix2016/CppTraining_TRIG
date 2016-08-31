ee786c4db40683340564acf2c556837e9bb6ce5c
zcphoenix2016 <13564240769@139.com>
2016-08-31 13:29:47 +0800

    iterator & reverse_iterator

---
+++ iterator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+
+int main()
+{
+    std::vector<int> vec{1, 2, 3, 4, 5, 6};
+
+    std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+
+    std::copy(vec.rbegin(), vec.rend(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+
+    return 0;
+}
