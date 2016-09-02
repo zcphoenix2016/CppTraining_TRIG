b192ca6eeeb09fde3b800d8cfffc2df3ab569a87
zcphoenix2016 <13564240769@139.com>
2016-09-02 12:52:52 +0800

    constexpr functions

--- meta_template.cpp
+++ meta_template.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
 
-template<unsigned char f>
+using u8 = unsigned char;
+using u64 = unsigned long long;
+
+template<u8 f>
 class Factorial
 {
 public:
-    static const unsigned long long value = (f * Factorial<f - 1>::value);
+    static const u64 value = (f * Factorial<f - 1>::value);
 };
 
 template<>
 class Factorial<0>
 {
 public:
-    static const unsigned long long value = 1;
+    static const u64 value = 1;
 };
+
+//Similar constexpr function
+constexpr u64 factorial(const u8 f)
+{
+    return (f == 0 ? 1 : f * factorial(f - 1));
+}
 
 int main()
 {
     std::cout << Factorial<7>::value << std::endl;
+    std::cout << factorial(7) << std::endl;
+    int arr[factorial(7)];
 
     return 0;
 }
